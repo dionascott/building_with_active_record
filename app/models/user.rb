8d969a6c738b9d5d053bc27cab798daad1c7061d
Dion Scott <diontscott@gmail.com>
2017-09-11 23:46:23 -0500

    Creating models and associations

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 50 },
+                   uniqueness: { case_sensitive: false }
+
+  has_many :posts
+  has_many :comments
+end
